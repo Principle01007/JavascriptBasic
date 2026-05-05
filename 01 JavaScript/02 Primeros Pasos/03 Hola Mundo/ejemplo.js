28b4c2b7c092463255297daedf3ca7da31bbe418
Principle <bemaidenprinciple01@gmail.com>
2026-05-05 15:14:00 -0500

    Agreagndo algunos ejemplos donde imprimimos en la consola una palabra, numeros y tambien como hacer comentarios que no hacen parte del cod sino que nos sirven para poder entender el cod, o indicaciones

---
+++ 01 JavaScript/02 Primeros Pasos/03 Hola Mundo/ejemplo.js
@@ -0,0 +1,4 @@
+console.log('Hello word');
+console.log(5);
+console.log({key: 'value'});
+console.log('Hola Universo');
---
+++ 01 JavaScript/02 Primeros Pasos/04 Comentarios/ejemplo2.js
@@ -0,0 +1,9 @@
+console.log('Hello word');
+//console.log(5);
+console.log([1,2,3]); // estos son una lista de numeros
+console.log({key: 'value'});
+
+/* Este
+es un comentario 
+multilinea
+*/
