b6c2c22364f10d3a5427887065eb0007726f890a
Principle <bemaidenprinciple01@gmail.com>
2026-05-05 16:27:09 -0500

    Aprendimos a susar var let y const aunque lo recomentable solo usar los dos ultimos de ahi vimos como podemos dar saltos de linea con variables variables definidas usando barra oblicua + n

---
+++ 01 JavaScript/03 Variables y tipos de datos/01 Variables/ejemplo1.js
@@ -0,0 +1,6 @@
+const a = 15;
+let b = 7;
+console.log(a, b);
+const pi = 3.1416;
+let radio = '50px';
+console.log(pi, radio);
---
+++ 01 JavaScript/03 Variables y tipos de datos/02 Texto/ejemplo02.js
@@ -0,0 +1,15 @@
+let primerLibro = "Iron Man II's\n";
+let SegundoEstilo = "Avengers";
+let texto = "Ella dijo: \"Hola mundo\" y se fue";
+let texto2 = 'It\'s a beautiful day';
+let texto3 = 'Harry \
+Potter \
+Castle ';
+
+var x = new String("hola");
+console.log(primerLibro, SegundoEstilo);
+// console.log(libro);
+// console.log(Movie);
+// let textoone = 'Harry \n\
+// Potter \nCastle '
+// console.log(textoone);
