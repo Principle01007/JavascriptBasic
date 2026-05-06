4f3e24c2ef5df1852e8715f631bfe5578b856dd8
Principle <bemaidenprinciple01@gmail.com>
2026-05-06 13:36:21 -0500

    Viendo como funciona y de definen las palabras y numeros que que porpiedades y metodos se pueden aplicar a estas de ahi vimos tmb la libreira Math que nos sirve para todo tipo de operaciones matematicas

---
+++ 01 JavaScript/03 Variables y tipos de datos/03 Métodos de Texto/ejemplo3.js
@@ -0,0 +1,11 @@
+// Metodos de texto
+let texto = "Pink Floyd";
+// texto tiene porpiedades y metodos
+// Propiedades> length: numero de caracteres
+// console.log(texto.length);
+// Metodo> toUpperCase: convierte todo los caracteres en mayuscula
+console.log(texto.toUpperCase())
+// Metodo> toLowerCase: lleva todo el texto a minuscula
+// console.log(texto.toLowerCase());
+// Metodo> includes: preguntamos si este texto incluye la palabra x si es asi nos dara TRUE:
+// console.log(texto.includes('Floyd1'));
---
+++ 01 JavaScript/03 Variables y tipos de datos/04 Número/ejemplo4.js
@@ -0,0 +1,18 @@
+let numero = 5 ;
+let decimales = 5.354647585777776543234567876543;
+let negativos = -10;
+let infinito = Infinity;
+const a = 123456789012345678901234567890n;
+const b = BigInt("123456789012345678901234567890");
+
+// console.log(numero);
+console.log(a);
+// console.log(negativos);
+// console.log(infinito);
+// console.log(-infinito);
+// console.log(NaN);
+
+// console.log(Math.PI);
+//  Redondea a cualquier decimal a un entero
+// console.log(Math.round(decimales)); 
+// console.log(Math.random());
